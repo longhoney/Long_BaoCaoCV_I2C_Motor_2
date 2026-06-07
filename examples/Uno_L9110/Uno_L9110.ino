69792775216105cfd5be01d455b8e9b094c79eb2
longhoney <long.711.sg@gmail.com>
2026-06-07 14:47:42 +0700

    L9110 van chua dieu khien duoc toc do

---
+++ examples/Uno_L9110/Uno_L9110.ino
@@ -0,0 +1,90 @@
+// 2026-06-07
+
+#define PIN_IN1 4 //! D4     --> A-IB
+#define PIN_IN2 5 //! D5 (~) --> A-IA
+#define PIN_IN3 6 //! D6 (~) --> B-IA
+#define PIN_IN4 7 //! D7     --> B-IB
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  L9110setup();
+  Serial.println("Điều khiển xe...");
+  // Serial.println("Đi thẳng tốc độ 100%.");
+  // car_fw(100, 100);
+  Serial.println("Đi thẳng tốc độ 50%.");
+  Serial.print("Speed A: "); Serial.print(speedA);
+  car_fw(50, 50);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void L9110setup() {
+  pinMode(PIN_IN1, OUTPUT);
+  pinMode(PIN_IN2, OUTPUT);
+  pinMode(PIN_IN3, OUTPUT);
+  pinMode(PIN_IN4, OUTPUT);
+
+  digitalWrite(PIN_IN1, LOW);
+  digitalWrite(PIN_IN2, LOW);
+  digitalWrite(PIN_IN3, LOW);
+  digitalWrite(PIN_IN4, LOW);
+}
+
+// Định nghĩa các hàm sẽ sử dụng trong chương trình
+int calculate_speed(int speed) {
+  // Đảm bảo giá trị tốc độ (%) nằm trong khoảng cho phép
+  speed = constrain(speed, 0, 100);
+
+  // Chuyển đổi giá trị tốc độ (%) sang (PWM)
+  speed = map(speed, 0, 100, 0, 255);
+
+  return speed;
+}
+
+void car_fw(int speedA, int speedB) {
+  // Xử lý giá trị tốc độ nhận được
+  speedA = calculate_speed(speedA);
+  speedB = calculate_speed(speedB);
+
+  motorA_fw(speedA); // Điều khiển motor kênh A quay thuận
+  motorB_fw(speedB); // Điều khiển motor kênh B quay thuận
+}
+
+void car_bw(int speedA, int speedB) {
+  // Xử lý giá trị tốc độ nhận được
+  speedA = calculate_speed(speedA);
+  speedB = calculate_speed(speedB);
+
+  motorA_bw(speedA); // Điều khiển motor kênh A quay ngược
+  motorB_bw(speedB); // Điều khiển motor kênh B quay ngược
+}
+
+void motorA_fw(int speed) {
+  speed = calculate_speed(speed);
+  digitalWrite(PIN_IN1, LOW);
+  analogWrite(PIN_IN2, 255 - speed); // ~PWM
+}
+
+void motorA_bw(int speed) {
+  speed = calculate_speed(speed);
+  digitalWrite(PIN_IN1, HIGH);
+  analogWrite(PIN_IN2, 255 - speed); // ~PWM
+}
+
+void motorB_fw(int speed) {
+  // Xử lý giá trị tốc độ nhận được
+  speed = calculate_speed(speed);
+  digitalWrite(PIN_IN4, LOW);
+  analogWrite(PIN_IN3, 255 - speed); // ~PWM
+}
+
+void motorB_bw(int speed) {
+  // Xử lý giá trị tốc độ nhận được
+  speed = calculate_speed(speed);
+  digitalWrite(PIN_IN4, LOW);
+  analogWrite(PIN_IN3, 255 - speed); // ~PWM
+}
