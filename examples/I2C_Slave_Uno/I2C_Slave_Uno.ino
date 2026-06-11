1de9657499fe7f3d7642f3d2cb6c690a800c9caf
longhoney <long.711.sg@gmail.com>
2026-06-12 05:18:07 +0700

    Test Wire.requestFrom() FAIL

--- examples/I2C_Slave_Uno/I2C_Slave_Uno.ino
+++ examples/I2C_Slave_Uno/I2C_Slave_Uno.ino
@@ -1,32 +1,44 @@
 #include <Wire.h>
+#include "Makerlabvn_SimpleMotor.h"
 
-/*
-
-    Y251216: Test I2C AGR12 Pressure Sensor 0~100kPa ASAIR với Vietduino Uno
-        - Clock I2C 50kHz
-        - Với data từ I2C, giá trị áp suất đã được tính toán trong cảm biến nên code này đọc cho cả 3 loại ngưỡng áp suất.
-            - I2C AGR12100C00 (0~100kPa): ~6.5kPA (Bơm chỗ Quí)
-            - I2C AGR12010C00 (0~10kPa): ~6.5kPA (Bơm chỗ Quí)
-
-        - Kết nối dây:
-            - Vietduino Uno    -----------------------Sensor
-                  5V      ----------------------------- 1
-                  GND     ----------------------------- 2
-                  SCL     ----------------------------- 3
-                  SDA     ----------------------------- 4
-        
-*/
+/* ------------------------------------------------------------------------- */
+/*                                   DEFINE                                  */
+/* ------------------------------------------------------------------------- */
+// Creator
+#define PIN_ENA 5    //! (~)
+#define PIN_IN1 7
+#define PIN_IN2 4
+#define PIN_IN3 9
+#define PIN_IN4 8
+#define PIN_ENB 6    //! (~)
 
 // Định nghĩa I2C Constants
 const uint8_t master_address = 0x50;
 // const uint8_t AGR12_I2C_ADDRESS = 0x50; // Địa chỉ I2C 7-bit (từ 0xA0/0xA1)
-const uint8_t CMD_MEASURE_HIGH = 0xAC; // Byte lệnh 1 [8]
-const uint8_t CMD_MEASURE_LOW = 0x12;  // Byte lệnh 2 [8]
-const int WAIT_TIME_MS = 80;           // Thời gian chờ sau khi gửi lệnh đo (ms) [8]
+// const uint8_t CMD_MEASURE_HIGH = 0xAC; // Byte lệnh 1 [8]
+// const uint8_t CMD_MEASURE_LOW = 0x12;  // Byte lệnh 2 [8]
+// const int WAIT_TIME_MS = 80;           // Thời gian chờ sau khi gửi lệnh đo (ms) [8]
+uint8_t _addressId = 0;   // Byte giá trị địa chỉ của slave
+uint8_t _modeId = 0;    // Byte chế độ điều khiển: DC Motor hay RC Servo
+uint8_t _index = 0;   // Byte quy định kênh sẽ điều khiển: A hoặc B
+uint8_t _pwm = 0;   // Byte giá trị tốc độ PWM 0-255
+uint8_t _dir = 0;   // Byte quy định chiều quay
+uint8_t _crc = 0;   // Byte giá trị checksum
+uint8_t calculated_crc = 0;
+// Khai bao bien gop byte
+// String dataHex = "";
+// Khai bao bien luu toc do dang 0-100%
+uint8_t speedPercent = 0;
+/* ------------------------------------------------------------------------- */
+/*                                   OBJECT                                  */
+/* ------------------------------------------------------------------------- */
+Makerlabvn_SimpleMotor demo;
+
 
 void setup() {
   Serial.begin(115200);
-  Wire.begin(); // Khởi tạo I2C bus
+  Wire.begin(0x40); // Khởi tạo I2C bus
+  demo.setup(PIN_ENA, PIN_IN1, PIN_IN2, PIN_IN3, PIN_IN4, PIN_ENB);
   Serial.println("Khoi tao Slave MKE Creator...");
 }
 
@@ -36,7 +48,7 @@ void loop() {
   } else {
     Serial.println("Loi doc du lieu tu Master.");
   }
-  delay(100); // Đọc mỗi 100 mili giây
+  delay(1000); // Đọc mỗi 100 mili giây
 }
 
 bool readData() {
@@ -44,22 +56,52 @@ bool readData() {
   uint8_t tempCount = Wire.requestFrom(master_address, 6);
   Serial.println("count: " + String(tempCount));
   if (tempCount == 6) {
-    uint8_t _addressId = Wire.read();   // Byte giá trị địa chỉ của slave
-    uint8_t _modeId = Wire.read();    // Byte chế độ điều khiển: DC Motor hay RC Servo
-    uint8_t _index = Wire.read();   // Byte quy định kênh sẽ điều khiển: A hoặc B
-    uint8_t _pwm = Wire.read();   // Byte giá trị tốc độ PWM 0-255
-    uint8_t _dir = Wire.read();   // Byte quy định chiều quay
-    uint8_t _crc = Wire.read();   // Byte giá trị checksum
+    _addressId = Wire.read();   // Byte giá trị địa chỉ của slave
+    _modeId = Wire.read();    // Byte chế độ điều khiển: DC Motor hay RC Servo
+    _index = Wire.read();   // Byte quy định kênh sẽ điều khiển: A hoặc B
+    _pwm = Wire.read();   // Byte giá trị tốc độ PWM 0-255
+    _dir = Wire.read();   // Byte quy định chiều quay
+    _crc = Wire.read();   // Byte giá trị checksum
+    
+    // Bước 2: Tính toán và kiểm tra CRC
+      // CRC  là tổng có các dữ liệu truyền đi [ADDRESS_ID] + [MODE_ID] + [INDEX] + [PWM] + [DIR]
+      calculated_crc = _addressId + _modeId + _index + _pwm + _dir;
+      if (calculated_crc != _crc) {
+      Serial.print("Loi CRC! Du lieu khong hop le. CRC nhan: ");
+      Serial.print(_crc, HEX);
+      Serial.print(", CRC tinh: ");
+      Serial.println(calculated_crc, HEX);
+      // return false;
+      }
+
+    // Bước 3: Chuyển đổi speedPWM sang speedPercent
+      // Thực hiện qua hàm cal_speed()
+    
+    // Bước 4: Hiển thị frame đã nhận được
+      printData();
+
+    // Bước 5: Điều khiển động cơ theo lệnh của Master
+      // - Index: Điều khiển motorA hay motorB
+      // - dir: 1 = quay thuan -> motorA_fw hoặc motorB_fw
+      if (_index == 0 && _dir == 1) {
+        // motorA_fw
+        Serial.println("Động cơ A quay thuận, tốc độ: " + String(speedPercent) + "%");
+        demo.motorA_fw(speedPercent);
+      } else if (_index == 0 && _dir == 0) {
+        Serial.println("Động cơ A quay nghịch, tốc độ: " + String(speedPercent) + "%");
+        demo.motorA_bw(speedPercent);
+      } else if (_index == 1 && _dir == 1) {
+        Serial.println("Động cơ B quay thuận, tốc độ: " + String(speedPercent) + "%");
+        demo.motorB_fw(speedPercent);
+      } else if (_index == 1 && _dir == 0) {
+        Serial.println("Động cơ B quay nghịch, tốc độ: " + String(speedPercent) + "%");
+        demo.motorB_bw(speedPercent);
+      }
+  } else {
+    Serial.println("Chi nhan duoc: " + String(tempCount) + " byte" + "(yeu cau 6 byte).");
+    return false;
   }
-  // Bước 2: Tính toán và kiểm tra CRC
-  // Bước 3: Phân tích nội dung Master gửi để thực hiện lệnh
-    // - Index: Điều khiển motorA hay motorB
-    // - dir: 1 = quay thuan -> motorA_fw hoặc motorB_fw
-    // - PWM: kiểm tra nằm trong 0-255? OK thì đưa vào biến speed của hàm motorA_fw hoặc motorB_fw
-  // Bước 4: Điều khiển động cơ theo lệnh của Master
-  // Bước 5: Hiển thị kết quả
-    // - Nhận được frame này
-    // - Động cơ A/B quay thuận/nghịch tốc độ xx%
+  
 }
 /*bool readPressure() {
   // BƯỚC 1: Gửi lệnh đo lường (0xAC 0x12) [8]
@@ -139,3 +181,25 @@ bool readData() {
   
   Serial.println("Khoi tao cam bien AGR12 I2C voi toc do 50 kHz...");
 }*/
+
+// Triển khai Bước 3: Chuyển đổi speedPWM -> Percentspeed
+  // - PWM: kiểm tra nằm trong 0-255? Chuyển đổi thành 0-100% để phù hợp với thư viện SimpleMotor
+  uint8_t cal_speed(uint8_t speedPWM) {
+    speedPWM = constrain(speedPWM, 0, 255);
+    speedPercent = map(speedPWM, 0, 255, 0, 100);
+    return speedPercent;
+  }
+
+// Hàm in dữ liệu
+  void printData() {
+    // uint8_t _addressId = Wire.read();   // Byte giá trị địa chỉ của slave
+    // uint8_t _modeId = Wire.read();    // Byte chế độ điều khiển: DC Motor hay RC Servo
+    // uint8_t _index = Wire.read();   // Byte quy định kênh sẽ điều khiển: A hoặc B
+    // uint8_t _pwm = Wire.read();   // Byte giá trị tốc độ PWM 0-255
+    // uint8_t _dir = Wire.read();   // Byte quy định chiều quay
+    // uint8_t _crc = Wire.read();   // Byte giá trị checksum
+    Serial.print("Frame đã nhận: ");
+    Serial.print(_addressId, HEX); Serial.print(" "); Serial.print(_modeId, HEX); Serial.print(" "); Serial.print(_index, HEX); Serial.print(" ");
+    Serial.print(_pwm, HEX); Serial.print(" "); Serial.print(_dir, HEX); Serial.print(" "); Serial.println(_crc, HEX);
+    Serial.print("CRC tinh lai: "); Serial.println(calculated_crc, HEX);
+  }
--- examples/I2C_Slave_Uno/readme.md
+++ examples/I2C_Slave_Uno/readme.md
@@ -1,12 +1,23 @@
 Master gửi tốc độ dạng PWM 0-255 (HEX) --> Tắt hàm cal_speed trên motorA_fw() luôn
 
 - Bước 1: Yêu cầu đọc 6 bytes từ master
-- Bước 2: Tính toán và kiểm tra CRC
-- Bước 3: Phân tích nội dung Master gửi để thực hiện lệnh
+- Bước 2: Tính toán và kiểm tra CRC. CRC là tổng các dữ liệu truyền đi: [ADDRESS_ID] + [MODE_ID] + [INDEX] + [PWM] + [DIR]
+- Bước 3: Chuyển đổi speedPWM sang speedPercent
+    - PWM: kiểm tra nằm trong 0-255? Chuyển đổi thành 0-100% để phù hợp với thư viện SimpleMotor
+- Bước 4: Hiển thị frame đã nhận được
+- Bước 5: Điều khiển motor theo lệnh của Master
     - Index: Điều khiển motorA hay motorB
     - dir: 1 = quay thuan -> motorA_fw hoặc motorB_fw
-    - PWM: kiểm tra nằm trong 0-255? OK thì đưa vào biến speed của hàm motorA_fw hoặc motorB_fw
-- Bước 4: Điều khiển động cơ theo lệnh của Master
-- Bước 5: Hiển thị kết quả
-    - Nhận được frame này
-    - Động cơ A/B quay thuận/nghịch tốc độ xx%
+
+### DC Motor
+Address: 0x40 = 64
+
+ModeId: 1
+
+Index: 0 (MA) va 1 (MB)
+
+PWM: 0 -> 255
+
+Dir: 1 (thuan) va 0 (nghich)
+
+[CHECKSUM_LOWBYTE] = [ADDRESS_ID] + [MODE_ID] + [INDEX] + [PWM] + [DIR]
